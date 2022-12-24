bb7ea5c638f74f83eea5b626fd696c406aa9f675
SnakerBone <raxxtq@gmail.com>
2022-12-24 22:10:45 +1100

    add custom writable book (book and quill)

---
+++ src/main/java/com/snaker/azcray/item/other/WritableBookModItem.java
@@ -0,0 +1,15 @@
+package com.snaker.azcray.item.other;
+
+import net.minecraft.item.WritableBookItem;
+
+public class WritableBookModItem extends WritableBookItem {
+    public WritableBookModItem(Properties properties) {
+        super(properties);
+    }
+
+    // writable book overrides
+
+    public WritableBookModItem() {
+        super(new Properties());
+    }
+}
---
+++ src/main/java/com/snaker/azcray/item/other/WrittenBookModItem.java
@@ -0,0 +1,16 @@
+package com.snaker.azcray.item.other;
+
+import net.minecraft.item.WrittenBookItem;
+
+public class WrittenBookModItem extends WrittenBookItem {
+    public WrittenBookModItem(Properties properties) {
+        super(properties);
+    }
+
+    // written book overrides
+
+    public WrittenBookModItem() {
+        super(new Properties());
+    }
+
+}
