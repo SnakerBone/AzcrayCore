4f87756f7fcfd1e1108d746a7b824d387d407f94
Taj Levinide <raxxtq@gmail.com>
2023-01-10 22:51:20 +1100

    imports refactor

--- src/main/java/com/snaker/azcray/Azcray.java
+++ src/main/java/com/snaker/azcray/Azcray.java
@@ -1,9 +1,10 @@
 package com.snaker.azcray;
 
-import com.snaker.azcray.init.EffectInit;
-import com.snaker.azcray.init.VanillaOverrideInit;
-import com.snaker.azcray.init.ItemInit;
-import com.snaker.azcray.init.SoundEventsInit;
+import com.snaker.azcray.data.Const;
+import com.snaker.azcray.init.AzcrayEffectInit;
+import com.snaker.azcray.init.AzcrayVanillaOverrideInit;
+import com.snaker.azcray.init.AzcrayItemInit;
+import com.snaker.azcray.init.AzcraySoundEventsInit;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -11,19 +12,18 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(Azcray.MOD_ID)
+@Mod(Const.MOD_ID)
 public class Azcray
 {
     public static final Logger LOGGER = LogManager.getLogger();
-    public static final String MOD_ID = "azcray";
 
     public Azcray()
     {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        ItemInit.ITEMS.register(eventBus);
-        VanillaOverrideInit.MINECRAFT_ITEM.register(eventBus);
-        EffectInit.EFFECTS.register(eventBus);
-        SoundEventsInit.SOUND_EVENTS.register(eventBus);
+        AzcrayItemInit.ITEMS.register(eventBus);
+        AzcrayVanillaOverrideInit.MINECRAFT_ITEM.register(eventBus);
+        AzcrayEffectInit.EFFECTS.register(eventBus);
+        AzcraySoundEventsInit.SOUND_EVENTS.register(eventBus);
         MinecraftForge.EVENT_BUS.register(this);
     }
 }
